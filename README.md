055937ad57ef947e629f24754782e4d8adac8d3f
Sergey Samsonov <osenvosem@gmail.com>
2019-01-06 20:20:48 +0300

    Add queue data structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# algorithms-and-data-structures
-Learning algorithms and data structures
+# Learning algorithms and data structures
+
+The repository is intended to log my process of learning algorithms and data structures.
+All code is written in Typescript.
---
+++ src/queues.ts
@@ -0,0 +1,34 @@
+const createQueue = () => {
+  const queue = [];
+
+  return {
+    enqueue(item) {
+      queue.unshift(item);
+    },
+    dequeue() {
+      return queue.pop();
+    },
+    peek() {
+      return queue[queue.length - 1];
+    },
+    get length() {
+      return queue.length;
+    },
+    isEmpty() {
+      return !queue.length;
+    }
+  };
+};
+
+const q = createQueue();
+
+q.enqueue("Buy some apples.");
+q.enqueue("Get a haircut.");
+q.enqueue("Go out for a run");
+
+q.dequeue(); // "Buy apples."
+q.dequeue(); // "Get a haircut."
+q.dequeue(); // "Go out for a run"
+
+console.log(q.isEmpty()); // true
+console.log(q.length); // 0
